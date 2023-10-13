e27fe7a1e3b7e2d94c4a4757da4b871943e0d302
Rama_Prakash <ramaprakeshmanugula@gmail.com>
2023-10-13 15:02:17 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,53 @@
+function changebg() {
+    var nav = document.getElementById('nav');
+    var scrollvalues = window.scrollY;
+    if (scrollvalues < 70) {
+        nav.classList.remove('bgcolor');
+    } else {
+        nav.classList.add('bgcolor');
+    }
+}
+window.addEventListener('scroll', changebg);
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hidden = document.querySelectorAll('.hidden');
+hidden.forEach((el) => observer.observe(el));
+
+const observer1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show1');
+        }
+        else {
+            entry.target.classList.remove('show1');
+        }
+    });
+});
+const hidden1 = document.querySelectorAll('.hidden1');
+hidden1.forEach((el) => observer1.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show2');
+        }
+        else {
+            entry.target.classList.remove('show2');
+        }
+    });
+});
+const hidden2 = document.querySelectorAll('.hidden2');
+hidden1.forEach((el) => observer1.observe(el));
